a733d58500dd6cf5b960b014154fb78893f283e5
bombtree <s.baumchen@gmail.com>
2017-04-27 19:20:28 -0700

    Add files via upload

--- json_rw.php
+++ json_rw.php
@@ -2,15 +2,19 @@
 error_reporting(E_ALL);
 $readRequest = $_POST["readRequest"];
 $writeRequest = $_POST["writeRequest"];
-$msg = $_POST["jsonToSend"];
-
+$writeVal = $_POST["writeValue"];
+$lightId = $_POST["id"];
+$handle;
+$msg = "";
 if(isset($readRequest)){
 	if ($readRequest == "weather"){
 		//read PiWeather.json
 		$handle = @fopen("/var/www/html/cash_json/PiWeather.json", "r");
 	} else if($readRequest == "lights"){
 		//read PiLights.json
-		$handle = @fopen("/var/www/html/cash_json/PiLights.json", "r");
+		$fileLocation = "/var/www/html/cash_json/PiLights" . $lightId . ".json";
+		
+		$handle = @fopen($fileLocation, "r");
 	} else if ($readRequest == "sensor"){
 		
 		$handle = @fopen("/var/www/html/cash_json/PiSensor.json", "r");
@@ -21,6 +25,7 @@ if(isset($readRequest)){
 	if($handle) {
 		while (($buf = fgets($handle)) !== false){
 			echo "$buf";
+			//$obj = jsonDecode($wholeMessage);
 		}
 		if (!feof($handle)){
 			echo "Error: fgets() fail\n";
@@ -31,15 +36,23 @@ if(isset($readRequest)){
 }
 
 if(isset($msg) && isset($writeRequest)){
-	if($writeRequest == "lights"){
-		$handle = fopen("/var/www/html/cash_json/WebLights.json", "w");
+	if($writeRequest == "light"){
+		$fileLocation = "/var/www/html/cash_json/WebLights" . $lightId . ".json";
+		$writeVal = (int)$writeVal;
+		$lightId = (int)$lightId;
+		$arr = array('module' => "lights", 'id' => $lightId, 'power' => $writeVal);
+		$msg = json_encode($arr);
+		$handle = fopen($fileLocation, "w");
 	} else if ($writeRequest == "alarm"){
+		$arr = array('module' => "alarm", 'state' => $writeVal);
+		$msg = json_encode($arr);
 		$handle = fopen("/var/www/html/cash_json/WebAlarm.json", "w");
 	}
 	if($handle){
 		fwrite($handle, $msg);
 		fclose($handle);
 	}
+	echo "Wrote to " . $fileLocation . "\n";
 }
 ?>
 
